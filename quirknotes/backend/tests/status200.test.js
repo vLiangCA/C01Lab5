51e14684ef9e199ca2d4244420425c482b68bd03
Vivian Liang <vivianliang255@gmail.com>
2024-02-15 13:28:41 -0500

    Initial codebase

---
+++ quirknotes/backend/tests/status200.test.js
@@ -0,0 +1,22 @@
+const SERVER_URL = "http://localhost:4000";
+
+test("/postNote - Post a note", async () => {
+  const title = "NoteTitleTest";
+  const content = "NoteTitleContent";
+
+  const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      content: content,
+    }),
+  });
+
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
+});
